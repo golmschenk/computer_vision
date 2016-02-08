d413d029608a835918b11ab5397cdc2455d9e893
Greg Olmschenk <golmschenk@gmail.com>
2016-02-08 17:27:16 -0500

    Add Seaborn to requirements

--- maximum_likelihood_learning_for_normal_distribution.py
+++ maximum_likelihood_learning_for_normal_distribution.py
@@ -1,4 +1,18 @@
+"""
+Code for the maximum likelihood learning for a normal distribution.
+"""
+
+
 def maximum_likelihood_learning_for_normal_distribution(training_data):
+    """
+    Determines the maximum likelihood parameters (mean and variance) for a normal distribution fit to a set of
+    training data.
+
+    :param training_data: The training data to fit the distribution to.
+    :type training_data: list[float]
+    :return: The mean and variance of the normal distribution.
+    :rtype: float, float
+    """
     mean = sum(training_data)/len(training_data)
     variance = sum([(training_datum - mean) ** 2 for training_datum in training_data])/len(training_data)
     return mean, variance
