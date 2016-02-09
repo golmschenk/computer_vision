357340d9b0b21eb4e6ad555dcb5897c6b13256ad
Greg Olmschenk <golmschenk@gmail.com>
2016-02-09 15:07:49 -0500

    Add display for maximum likelihood normal distribution

--- maximum_likelihood_learning_for_normal_distribution.py
+++ maximum_likelihood_learning_for_normal_distribution.py
@@ -1,9 +1,15 @@
 """
 Code for the maximum likelihood learning for a normal distribution.
 """
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy.stats import norm
+import seaborn as sns
+
+import constant
 
 
-def maximum_likelihood_learning_for_normal_distribution(training_data):
+def generate_parameters(training_data):
     """
     Determines the maximum likelihood parameters (mean and variance) for a normal distribution fit to a set of
     training data.
@@ -16,3 +22,25 @@ def maximum_likelihood_learning_for_normal_distribution(training_data):
     mean = sum(training_data)/len(training_data)
     variance = sum([(training_datum - mean) ** 2 for training_datum in training_data])/len(training_data)
     return mean, variance
+
+
+def display_model(training_data):
+    """
+    Displays a plot of the maximum likelihood distribution along with the input data.
+
+    :param training_data: The training data to fit the distribution to.
+    :type training_data: list[float]
+    """
+    mean, variance = generate_parameters(training_data)
+    sigma = variance ** 0.5
+    for training_datum in training_data:
+        plt.plot([training_datum, training_datum], [0.0, norm.pdf(training_datum, mean, sigma)])
+    plotting_space = np.linspace(norm.ppf(0.01, mean, sigma),
+                                 norm.ppf(0.99, mean, sigma),
+                                 constant.plot_samples)
+    plt.plot(plotting_space, norm.pdf(plotting_space, mean, sigma))
+    plt.show()
+
+
+if __name__ == '__main__':
+    display_model([0.5, 0.7, 0.8, 1.0, 1.2, 1.3])
--- tests/test_maximum_likelihood_learning_for_normal_distribution.py
+++ tests/test_maximum_likelihood_learning_for_normal_distribution.py
@@ -1,10 +1,35 @@
-from maximum_likelihood_learning_for_normal_distribution import maximum_likelihood_learning_for_normal_distribution
+from unittest.mock import patch
+import numpy as np
+import scipy
+
+import constant
+from maximum_likelihood_learning_for_normal_distribution import generate_parameters, display_model
 
 
-def test_maximum_likelihood_learning_for_normal_distribution():
-    training_data = [0.5, 0.7, 0.8, 1.0, 1.2, 1.3]
+class TestMaximumLikelihoodLearningForNormalDistribution:
+    def test_parameters_are_correctly_generated(self):
+        training_data = [0.5, 0.7, 0.8, 1.0, 1.2, 1.3]
 
-    mean, variance = maximum_likelihood_learning_for_normal_distribution(training_data)
+        mean, variance = generate_parameters(training_data)
 
-    assert abs(mean - 0.9166) < 0.0001
-    assert abs(variance - 0.0780) < 0.0001
+        assert abs(mean - 0.9166) < 0.0001
+        assert abs(variance - 0.0780) < 0.0001
+
+    @patch('maximum_likelihood_learning_for_normal_distribution.generate_parameters')
+    @patch('maximum_likelihood_learning_for_normal_distribution.plt')
+    def test_the_model_can_be_displayed(self, mock_plt, mock_generate_parameters):
+        mean = 0.9166
+        sigma = 0.0780 ** 0.5
+        mock_generate_parameters.return_value = (0.9166, 0.0780)
+        training_data = [0.5, 0.7, 0.8, 1.0, 1.2, 1.3]
+
+        display_model(training_data)
+
+        assert (([0.7, 0.7], [0.0, scipy.stats.norm.pdf(0.7, mean, sigma)]),) in mock_plt.plot.call_args_list
+        assert (([1.2, 1.2], [0.0, scipy.stats.norm.pdf(1.2, mean, sigma)]),) in mock_plt.plot.call_args_list
+        plotting_space = np.linspace(scipy.stats.norm.ppf(0.01, mean, sigma),
+                                     scipy.stats.norm.ppf(0.99, mean, sigma),
+                                     constant.plot_samples)
+        distribution = scipy.stats.norm.pdf(plotting_space, mean, sigma)
+        assert np.array_equal((plotting_space, distribution), mock_plt.plot.call_args_list[6][0])
+        assert mock_plt.show.called
